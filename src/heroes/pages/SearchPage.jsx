31de1fe7c90b530ba713b68c24fa8792e744e62a
MatiasCasco <matiascasco_96@hotmail.com>
2025-03-16 19:17:09 -0300

    Se agrego la iteraccion que faltaba segun la busqueda

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -19,11 +19,11 @@ export const SearchPage = () => {
 
     const onSearchSubmit = (e) => {
         e.preventDefault();
-        if ( searchText.trim().length <= 1 ) return;
+        /*if ( searchText.trim().length <= 1 ) return;*/
         console.log(searchText);
         navigate(`?q=${searchText}`);
 
-        onResetForm();
+      /*onResetForm();*/
     }
 
     return (
@@ -55,14 +55,21 @@ export const SearchPage = () => {
                 <div className="col-7">
                     <h4>Results</h4>
                     <hr/>
-                    <div className="alert alert-info">
-                        Search a hero...
-                    </div>
+                    {
+                        q === ''?
+                            <div className="alert alert-info">
+                                 Search a hero...
+                            </div>
+                            : heroes.length === 0 &&  (
+                                <div className="alert alert-danger">
+                                    No hero with <b>{q}</b>
+                                </div>
+                        )
+                     }
 
 
-                    <div className="alert alert-danger">
-                        No hero with <b>{q}</b>
-                    </div>
+
+
 
                     {
                         heroes.map(hero => (
